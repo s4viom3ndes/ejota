2657d6f26b71614fd615b018abb9283f3247ffbf
s4viom3ndes <103843018+s4viom3ndes@users.noreply.github.com>
2023-08-01 11:47:00 -0300

    Normalizando os dados
    Normalizando os dados que foram retirados da coluna: user_id, e inseridos em lista.

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ caminho = "likes.csv"
 dataframe = pd.read_csv(caminho)
 
 print(dataframe)
+
+# pegando a coluna User_id e inserindo em uma lista
+listUser_id = dataframe["user_id"].tolist()
+
+print(listUser_id)
+
+# normalizando dados inseridos na lista
+
+# calculando a media
+soma = 0
+
+soma = sum(listUser_id)
+print(soma)
+media = soma/len(listUser_id)
+print(media)
+
+menor = min(listUser_id)
+maior = max(listUser_id)
+
+normalizacao = (listUser_id(1) - menor)/(maior - menor)
+print(f'normalizacao = {normalizacao}')
